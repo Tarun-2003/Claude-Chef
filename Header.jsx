fd3f26d79cd169f704fcf2fcc8b39610d466d99f
kmvucd <tharunindira@gmail.com>
2026-06-02 12:39:22 +0100

    Add React components and update app

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return (
+        <>
+        <div className="main"> 
+            <img src="./images/chef-claude-icon.png" width="50" height="50" alt="Chef Claude" />
+
+            <h3>Chef Claude</h3>
+        </div>
+        
+        
+        
+        </>
+    )
+
+}
+export default Header;
